86676baad2c3c761ecf2fed72a09f82edd1261ac
chowon hasan <chowonhasan7@gmail.com>
2024-12-11 12:27:11 +0600

    dfsdfhsj

--- p-2.js
+++ p-2.js
@@ -1,3 +1,4 @@
+//
 var createCounter = function (n) {
   return function () {
     // return n++;
@@ -5,7 +6,16 @@ var createCounter = function (n) {
   };
 };
 
-const counter = createCounter(5);
-counter();
+const counter = createCounter(1);
+// counter();
+// counter();
 
 console.log(counter());
+// console.log(counter());
+
+// key points
+
+// closures
+// post increament - n++ and n = n + 1
+// n++ that will return current value and increament by 1 later
+// n = n+1 that will update the value of n and then return the update value
