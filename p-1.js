7600b7970a8725c474cc477444947c10ad787016
chowon hasan <chowonhasan7@gmail.com>
2024-12-11 12:11:44 +0600

    sdfs

--- p-1.js
+++ p-1.js
@@ -1,3 +1,4 @@
+// Function returns function
 var createHelloWorld = function (...args) {
   return function (...args) {
     return "Hello world";
@@ -8,3 +9,17 @@ const f = createHelloWorld();
 f({}, ["a", "b"], 42);
 
 console.log(f());
+
+// Learning keypoint
+// rest param can take any kind of input by ...args or ...anything
+// Closures - its a feature of js that can access and remember outer function and scope after the function are executed.
+
+// another example
+const resturent = function (...order) {
+  return function (...takingOrder) {
+    return "only bread";
+  };
+};
+
+const ordered = resturent("vhuna kishori");
+console.log(ordered());
